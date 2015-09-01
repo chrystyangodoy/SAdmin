a4a4f0e404fa13e19539d2c222105dd5dd087760
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-01 13:35:54 -0300

    Ajustes Template 02

--- victor.php
+++ victor.php
@@ -1,8 +1,2 @@
 <?php
 echo 'Funfou!!!!';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
