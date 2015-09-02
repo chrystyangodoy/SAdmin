63597034beb0709663ac83e7dae5ef771c207e82
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-02 01:18:13 -0300

    Ajustes Template 02

--- UsuarioList.php
+++ UsuarioList.php
@@ -1,5 +1,10 @@
 <?php
 
+//if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true)) { 
+//    unset($_SESSION['login']); 
+//    unset($_SESSION['senha']); 
+//    header('location:index.php'); } $logado = $_SESSION['login'];
+
 require_once 'smarty.php';
 require_once './actions/aUsuario.php';
 $user = new aUsuario();
