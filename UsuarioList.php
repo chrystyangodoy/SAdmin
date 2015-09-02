77d6fe8e8bff6ade8a0d093bbb5357271a8e02fb
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-02 00:15:35 -0300

    Ajustes Template 02

--- UsuarioList.php
+++ UsuarioList.php
@@ -1,18 +1,19 @@
 <?php
+
 require_once 'smarty.php';
 require_once './actions/aUsuario.php';
 $user = new aUsuario();
 
-if(isset($_GET['del'])){
+if (isset($_GET['del'])) {
     $user->setID_Usuario($_GET['del']);
-    if($user->delete()){
+    
+    if ($user->delete()) {
         echo "Registro excluído com sucesso!";
-    }else{
+    } else {
         echo("Erro na excluído!");
     }
-        
 }
 
-$smarty -> assign("lista",$user->selectInnerGrupo());
+$smarty->assign("lista", $user->selectInnerGrupo());
 
 $smarty->display('./View/UsuarioList.html');
