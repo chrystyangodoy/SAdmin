d8bb5985e164cea6a231afab0115c88b2c117b00
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-01 22:06:38 -0300

    Ajustes Template 02

--- CadastroUsuario.php
+++ CadastroUsuario.php
@@ -4,14 +4,20 @@ require_once 'smarty.php';
 require ('./actions/aUsuario.php');
 require ('./actions/aGrupoUsuario.php');
 $grupo = new aGrupoUsuario();
-
 $user = new aUsuario();
 
 
-if(isset($_GET['del'])){
-    $user->setID_Usuario($_GET['del']);
-    $user->delete();
+if(isset($_POST['Cadastrar'])){
+    $user->setDSC_Login($_POST['DSC_Login']);
+    $user->setDSC_Senha($_POST['DSC_Senha']);
+    $user->setDTM_Inicio($_POST['DTM_Inicio']);
+    $user->setDTM_Fim($_POST['DTM_Fim']);
+    $user->setID_SEG_Grupo($_POST['ID_SEG_Grupo']);
+    $user->insert();
+    echo "Usuário inserido com sucesso!";
+}else {
+    echo "Não foi possível inserir usuário!";
 }
-//$smarty -> assign("listGrupo",$grupo->select());
+$smarty -> assign("listGrupo",$grupo->select());
 
 $smarty->display('./View/UsuarioInsert.html');
