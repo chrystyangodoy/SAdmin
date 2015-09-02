404b8b1ae8d45eacb68e56f427cf4717f859b489
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-02 02:38:05 -0300

    Ajustes Template 02

--- CadastroUsuario.php
+++ CadastroUsuario.php
@@ -3,20 +3,20 @@ require_once 'smarty.php';
 
 require ('./actions/aUsuario.php');
 require ('./actions/aGrupoUsuario.php');
-$TpPart = new aGrupoUsuario();
-$TpPart = new aUsuario();
+$grupo = new aGrupoUsuario();
+$user= new aUsuario();
 
 if(isset($_POST['Cadastrar'])){
-    $TpPart->setDSC_Login($_POST['DSC_Login']);
-    $TpPart->setDSC_Senha($_POST['DSC_Senha']);
-    $TpPart->setDTM_Inicio($_POST['DTM_Inicio']);
-    $TpPart->setDTM_Fim($_POST['DTM_Fim']);
-    $TpPart->setID_SEG_Grupo($_POST['ID_SEG_Grupo']);
-    $TpPart->insert();
+    $user->setDSC_Login($_POST['DSC_Login']);
+    $user->setDSC_Senha($_POST['DSC_Senha']);
+    $user->setDTM_Inicio($_POST['DTM_Inicio']);
+    $user->setDTM_Fim($_POST['DTM_Fim']);
+    $user->setID_SEG_Grupo($_POST['ID_SEG_Grupo']);
+    $user->insert();
     echo "Usuário inserido com sucesso!";
 }else {
     echo "Não foi possível inserir usuário!";
 }
-$smarty -> assign("listGrupo",$TpPart->select());
+$smarty -> assign("listGrupo",$grupo->select());
 
 $smarty->display('./View/UsuarioInsert.html');
