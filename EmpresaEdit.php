6c4ddf814db0a8575aed8e8a6e7d3f9ed246c1da
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-03 00:55:11 -0300

    Ajustes Template 02

--- EmpresaEdit.php
+++ EmpresaEdit.php
@@ -3,29 +3,13 @@
 require_once 'smarty.php';
 
 require ('./actions/aBsc_Empresa.php');
-$empresa = new aBsc_Empresa();
-$empresa->setID_Empresa($_GET['alt']);
-$empresa->load();
+$emp = new aBsc_Empresa();
+$emp->setID_Empresa(2);
+$emp->load();
 
-$regEmpresa = array('COD_CNPJ' => $empresa->getCOD_CNPJ(), 'DSC_RazaoSocial' => $empresa->getDSC_RazaoSocial(), 'DSC_Endereco' => $empresa->getDSC_Endereco(), 'DSC_Bairro' => $empresa->getDSC_Bairro(), 'DSC_Cidade' => $empresa->getDSC_Cidade(), 'NUM_CEP' => $empresa->getNUM_CEP(), 'NUM_InscricaoEstadual' => $empresa->getNUM_InscricaoEstadual(), 'NUM_Fone' => $empresa->getNUM_Fone(), 'NUM_FAX' => $empresa->getNUM_FAX(), 'DSC_EMAIL' => $empresa->getDSC_EMAIL(), 'COD_TipoEstado' => $empresa->getCOD_TipoEstado());
+$regEmpresa = array('COD_CNPJ' => $emp->getCOD_CNPJ(), 'DSC_RazaoSocial' => $emp->getDSC_RazaoSocial(), 'DSC_Endereco' => $emp->getDSC_Endereco(), 'DSC_Bairro' => $emp->getDSC_Bairro(), 'DSC_Cidade' => $emp->getDSC_Cidade(), 'NUM_CEP' => $emp->getNUM_CEP(), 'NUM_InscricaoEstadual' => $emp->getNUM_InscricaoEstadual(), 'NUM_Fone' => $emp->getNUM_Fone(), 'NUM_FAX' => $emp->getNUM_FAX(), 'DSC_EMAIL' => $emp->getDSC_EMAIL(), 'COD_TipoEstado' => $emp->getCOD_TipoEstado());
 
-if (isset($_POST['Salvar'])) {
-    $empresa->setCOD_CNPJ($_POST['COD_CNPJ']);
-    $empresa->setDSC_RazaoSocial($_POST['DSC_RazaoSocial']);
-    $empresa->setDSC_Endereco($_POST['DSC_Endereco']);
-    $empresa->setDSC_Bairro($_POST['DSC_Bairro']);
-    $empresa->setDSC_Cidade($_POST['DSC_Cidade']);
-    $empresa->setNUM_CEP($_POST['NUM_CEP']);
-    $empresa->setNUM_InscricaoEstadual($_POST['NUM_InscricaoEstadual']);
-    $empresa->setNUM_Fone($_POST['NUM_Fone']);
-    $empresa->setNUM_FAX($_POST['NUM_FAX']);
-    $empresa->setDSC_EMAIL($_POST['DSC_EMAIL']);
-    $empresa->setCOD_TipoEstado($_POST['COD_TipoEstado']);
-    $empresa->update();
-    echo "Empresa inseridas com sucesso!";
+foreach ($regEmpresa as $value) {
+    echo "$value -<br>";
 }
 
-
-$smarty->assign('regEmp', $regEmpresa);
-
-$smarty->display('./View/EmpresaEdit.html');
