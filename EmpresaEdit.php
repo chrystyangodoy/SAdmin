83abec4a418739e7d5c5492c6f5110d2ad92ad47
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-03 01:14:32 -0300

    Ajustes Template 02

--- EmpresaEdit.php
+++ EmpresaEdit.php
@@ -12,18 +12,18 @@ $regEmpresa = array('COD_CNPJ' => $emp->getCOD_CNPJ(), 'DSC_RazaoSocial' => $emp
 $smarty->assign('regEmp', $regEmpresa);
 
 if (isset($_POST['Salvar'])) {
-    $empresa->setCOD_CNPJ($_POST['COD_CNPJ']);
-    $empresa->setDSC_RazaoSocial($_POST['DSC_RazaoSocial']);
-    $empresa->setDSC_Endereco($_POST['DSC_Endereco']);
-    $empresa->setDSC_Bairro($_POST['DSC_Bairro']);
-    $empresa->setDSC_Cidade($_POST['DSC_Cidade']);
-    $empresa->setNUM_CEP($_POST['NUM_CEP']);
-    $empresa->setNUM_InscricaoEstadual($_POST['NUM_InscricaoEstadual']);
-    $empresa->setNUM_Fone($_POST['NUM_Fone']);
-    $empresa->setNUM_FAX($_POST['NUM_FAX']);
-    $empresa->setDSC_EMAIL($_POST['DSC_EMAIL']);
-    $empresa->setCOD_TipoEstado($_POST['COD_TipoEstado']);
-    $empresa->update();
+    $emp->setCOD_CNPJ($_POST['COD_CNPJ']);
+    $emp->setDSC_RazaoSocial($_POST['DSC_RazaoSocial']);
+    $emp->setDSC_Endereco($_POST['DSC_Endereco']);
+    $emp->setDSC_Bairro($_POST['DSC_Bairro']);
+    $emp->setDSC_Cidade($_POST['DSC_Cidade']);
+    $emp->setNUM_CEP($_POST['NUM_CEP']);
+    $emp->setNUM_InscricaoEstadual($_POST['NUM_InscricaoEstadual']);
+    $emp->setNUM_Fone($_POST['NUM_Fone']);
+    $emp->setNUM_FAX($_POST['NUM_FAX']);
+    $emp->setDSC_EMAIL($_POST['DSC_EMAIL']);
+    $emp->setCOD_TipoEstado($_POST['COD_TipoEstado']);
+    $emp->update();
     echo "Empresa inseridas com sucesso!";
 }
 
