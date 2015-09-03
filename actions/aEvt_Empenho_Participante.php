48630a49ccb508cde9ba1bf8e758637b192de972
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-03 19:46:02 -0300

    Ajustes 03-09-2015 - Noite

--- actions/aEvt_Empenho_Participante.php
+++ actions/aEvt_Empenho_Participante.php
@@ -4,23 +4,23 @@ require './model/mEvt_Empenho_Participante.php';
 
 class aEvt_Empenho_Participante extends mEvt_Empenho_Participante {
 
-    protected $sqlInsert = "INSERT INTO evt_empenho_participante(COD_CNPJ, DSC_RazaoSocial, DSC_Endereco, DSC_Bairro, DSC_Cidade, NUM_CEP, NUM_InscricaoEstadual, NUM_Fone, NUM_FAX, DSC_EMAIL, COD_TipoEstado) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')";
-    protected $sqlUpdate = "update evt_empenho_participante set COD_CNPJ= '%s', DSC_RazaoSocial= '%s', DSC_Endereco= '%s', DSC_Bairro= '%s', DSC_Cidade= '%s', NUM_CEP= '%s', NUM_InscricaoEstadual= '%s', NUM_Fone= '%s', NUM_FAX= '%s', DSC_EMAIL= '%s', COD_TipoEstado= '%s' where ID_Empresa = '%s'";
-    protected $sqlDelete = "delete from evt_empenho_participante where ID_Empresa = '%s'";
+    protected $sqlInsert = "INSERT INTO evt_empenho_participante(COD_CPF, DSC_Nome, SIT_USUFRUI, VLR_Consumido, ID_EVT_Empenho) VALUES ('%s','%s','%s','%s','%s')";
+    protected $sqlUpdate = "update evt_empenho_participante set COD_CPF='%s', DSC_Nome='%s', SIT_USUFRUI='%s', VLR_Consumido='%s', ID_EVT_Empenho='%s' where ID_Empenho_Participante = '%s'";
+    protected $sqlDelete = "delete from evt_empenho_participante where ID_Empenho_Participante = '%s'";
     protected $sqlSelect = "select * from evt_empenho_participante where 1=1 %s %s";
 
     public function insert() {
-        $sql = sprintf($this->sqlInsert, $this->getCOD_CNPJ(), $this->getDSC_RazaoSocial(), $this->getDSC_Endereco(), $this->getDSC_Bairro(), $this->getDSC_Cidade(), $this->getNUM_CEP(), $this->getNUM_InscricaoEstadual(), $this->getNUM_Fone(), $this->getNUM_FAX(), $this->getDSC_EMAIL(), $this->getCOD_TipoEstado());
+        $sql = sprintf($this->sqlInsert, $this->getCOD_CPF(), $this->getDSC_Nome(), $this->getSIT_USUFRUI(), $this->getVLR_Consumido(), $this->getID_EVT_Empenho());
         return $this->RunQuery($sql);
     }
 
     public function update() {
-        $sql = sprintf($this->sqlUpdate, $this->getCOD_CNPJ(), $this->getDSC_RazaoSocial(), $this->getDSC_Endereco(), $this->getDSC_Bairro(), $this->getDSC_Cidade(), $this->getNUM_CEP(), $this->getNUM_InscricaoEstadual(), $this->getNUM_Fone(), $this->getNUM_FAX(), $this->getDSC_EMAIL(), $this->getCOD_TipoEstado());
+        $sql = sprintf($this->sqlUpdate, $this->getCOD_CPF(), $this->getDSC_Nome(), $this->getSIT_USUFRUI(), $this->getVLR_Consumido(), $this->getID_EVT_Empenho());
         return $this->RunQuery($sql);
     }
 
     public function delete() {
-        $sql = sprintf($this->sqlDelete, $this->getID_Empresa());
+        $sql = sprintf($this->sqlDelete, $this->getID_Empenho_Participante());
         return $this->RunQuery($sql);
     }
 
@@ -30,19 +30,12 @@ class aEvt_Empenho_Participante extends mEvt_Empenho_Participante {
     }
 
     public function load() {
-        $rs = $this->select(sprintf("and ID_Empresa='%s'", $this->getID_Empresa()));
-        $this->getID_Empresa($rs[0]['ID_Empresa']);
-        $this->getID_Empresa($rs[0]['COD_CNPJ']);
-        $this->getID_Empresa($rs[0]['DSC_RazaoSocial']);
-        $this->getID_Empresa($rs[0]['DSC_Endereco']);
-        $this->getID_Empresa($rs[0]['DSC_Bairro']);
-        $this->getID_Empresa($rs[0]['DSC_Cidade']);
-        $this->getID_Empresa($rs[0]['NUM_CEP']);
-        $this->getID_Empresa($rs[0]['NUM_InscricaoEstadual']);
-        $this->getID_Empresa($rs[0]['NUM_Fone']);
-        $this->getID_Empresa($rs[0]['NUM_FAX']);
-        $this->getID_Empresa($rs[0]['DSC_EMAIL']);
-        $this->getID_Empresa($rs[0]['COD_TipoEstado']);
+        $rs = $this->select(sprintf("and ID_Empenho_Participante='%s'", $this->getID_Empenho_Participante()));
+        $this->getID_Empenho_Participante($rs[0]['ID_Empenho_Participante']);
+        $this->getID_Empenho_Participante($rs[0]['DSC_RazaoSocial']);
+        $this->getID_Empenho_Participante($rs[0]['DSC_Endereco']);
+        $this->getID_Empenho_Participante($rs[0]['DSC_Bairro']);
+        $this->getID_Empenho_Participante($rs[0]['DSC_Cidade']);
         return $this;
     }
 
