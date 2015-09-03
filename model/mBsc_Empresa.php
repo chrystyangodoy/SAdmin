1199f84196606963813eee21170375831c1bcbca
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-02 21:48:37 -0300

    Ajustes Template 02

--- model/mBsc_Empresa.php
+++ model/mBsc_Empresa.php
@@ -32,13 +32,15 @@ class mBsc_Empresa extends dbConnection {
     public function getCOD_CNPJ() {
         return $this->COD_CNPJ;
     }
-public function setDSC_RazaoSocial($DSC_RazaoSocial) {
+
+    public function setDSC_RazaoSocial($DSC_RazaoSocial) {
         $this->DSC_RazaoSocial = $DSC_RazaoSocial;
     }
 
     public function getDSC_RazaoSocial() {
         return $this->DSC_RazaoSocial;
     }
+
     public function setDSC_Endereco($DSC_Endereco) {
         $this->DSC_Endereco = $DSC_Endereco;
     }
@@ -46,13 +48,15 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getDSC_Endereco() {
         return $this->DSC_Endereco;
     }
-     public function setDSC_Bairro($DSC_Bairro) {
+
+    public function setDSC_Bairro($DSC_Bairro) {
         $this->DSC_Bairro = $DSC_Bairro;
     }
 
     public function getDSC_Bairro() {
         return $this->DSC_Bairro;
     }
+
     public function setDSC_Cidade($DSC_Cidade) {
         $this->DSC_Cidade = $DSC_Cidade;
     }
@@ -60,6 +64,7 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getDSC_Cidade() {
         return $this->DSC_Cidade;
     }
+
     public function setNUM_CEP($NUM_CEP) {
         $this->NUM_CEP = $NUM_CEP;
     }
@@ -67,6 +72,7 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getNUM_CEP() {
         return $this->NUM_CEP;
     }
+
     public function setNUM_InscricaoEstadual($NUM_InscricaoEstadual) {
         $this->NUM_InscricaoEstadual = $NUM_InscricaoEstadual;
     }
@@ -74,6 +80,7 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getNUM_InscricaoEstadual() {
         return $this->NUM_InscricaoEstadual;
     }
+
     public function setNUM_Fone($NUM_Fone) {
         $this->NUM_Fone = $NUM_Fone;
     }
@@ -81,6 +88,7 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getNUM_Fone() {
         return $this->NUM_Fone;
     }
+
     public function setNUM_FAX($NUM_FAX) {
         $this->NUM_Fone = $NUM_FAX;
     }
@@ -88,6 +96,7 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getNUM_FAX() {
         return $this->NUM_FAX;
     }
+
     public function setDSC_EMAIL($DSC_EMAIL) {
         $this->DSC_EMAIL = $DSC_EMAIL;
     }
@@ -95,6 +104,7 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getDSC_EMAIL() {
         return $this->DSC_EMAIL;
     }
+
     public function setCOD_TipoEstado($COD_TipoEstado) {
         $this->COD_TipoEstado = $COD_TipoEstado;
     }
@@ -102,6 +112,5 @@ public function setDSC_RazaoSocial($DSC_RazaoSocial) {
     public function getCOD_TipoEstado() {
         return $this->COD_TipoEstado;
     }
-    
-    
+
 }
