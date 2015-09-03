1a69439bb98a0b4ba2d788506675474847b949f0
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-03 00:40:15 -0300

    Ajustes Template 02

--- EmpresaList.php
+++ EmpresaList.php
@@ -9,11 +9,8 @@ if (isset($_GET['del'])) {
     $empresa->setID_EVT($_GET['del']);
     $empresa->delete();
 }
-if (isset($_GET['alt'])) {
-    $smarty->display('EmpresaEdit.php');
-} else {
+
     $smarty->assign("lista", $empresa->select());
 
     $smarty->display('./View/EmpresaList.html');
-}
 
