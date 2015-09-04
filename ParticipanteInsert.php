0293ff4af3ab32ca6058402b89897a97017abe88
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-04 00:23:12 -0300

    Ajustes 03-09-2015 - Noite

--- ParticipanteInsert.php
+++ ParticipanteInsert.php
@@ -5,9 +5,11 @@ require_once 'smarty.php';
 require ('./actions/aBsc_Participante.php');
 require_once './actions/aBsc_Empresa.php';
 require_once './actions/aBsc_Profissao.php';
+require_once './actions/aUsuario.php';
 $partic = new aBsc_Participante();
 $emp = new aBsc_Empresa();
 $prof = new aBsc_Profissao();
+
 
 if (isset($_POST['Cadastrar'])) {
     $partic->setCOD_CPF($_POST['COD_CPF']);
@@ -31,6 +33,40 @@ if (isset($_POST['Cadastrar'])) {
 } else {
     echo "Não foi possível inserir participante!";
 }
+if (isset($_GET['Cadastre-se'])) {   
+    $partic->setCOD_CPF($_POST['COD_CPF']);
+    $partic->setCOD_RG($_POST['COD_RG']);
+    $partic->setDSC_Nome($_POST['DSC_Nome']);
+    $partic->setDSC_Endereco($_POST['DSC_Endereco']);
+    $partic->setDSC_Bairro($_POST['DSC_Bairro']);
+    $partic->setDSC_Cidade($_POST['DSC_Cidade']);
+    $partic->setNUM_CEP($_POST['NUM_CEP']);
+    $partic->setNUM_Fone($_POST['NUM_Fone']);
+    $partic->setNUM_Celular($_POST['NUM_Celular']);
+    $partic->setNUM_FAX($_POST['NUM_FAX']);
+    $partic->setDSC_Profissao_Especialidade($_POST['DSC_Profissao_Especialidade']);
+    $partic->setDSC_Email($_POST['DSC_Email']);
+    $partic->setNUM_Registro($_POST['NUM_Registro']);
+    $partic->setCOD_Tipo_Estado($_POST['COD_Tipo_Estado']);
+    $partic->setID_BSC_Empresa($_POST['ID_BSC_Empresa']);
+    $partic->setID_BSC_Profissao($_POST['ID_BSC_Profissao']);
+    $partic->insert();
+    
+    
+    $usu = new aUsuario();
+    
+    $data = new DateTime();
+    $datafim = new DateTime();
+    $datafim -> modify("+7 day");
+    
+    $user->setDSC_Login($_POST['COD_CPF']);
+    $user->setDSC_Senha($_POST['DSC_Senha']);
+    $user->setDTM_Inicio($data);
+    $user->setDTM_Fim($datafim);
+    $user->setID_SEG_Grupo(99);
+    $user->insert();
+}
+
 $smarty -> assign("listEmp",$emp->select());
 $smarty -> assign("listProf",$prof->select());
 
--- actions/aBsc_Participante.php
+++ actions/aBsc_Participante.php
@@ -5,10 +5,12 @@ require './model/mBsc_Participante.php';
 class aBsc_Participante extends mBsc_Participante {
 
     protected $sqlInsert = "INSERT INTO bsc_participante(COD_CPF,COD_RG,DSC_Nome,DSC_Endereco,DSC_Bairro,DSC_Cidade,NUM_CEP,NUM_Fone,NUM_Celular,NUM_FAX,DSC_Profissao_Especialidade,DSC_Email,NUM_Registro,COD_Tipo_Estado,ID_BSC_Empresa,ID_BSC_Profissao) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')";
-    protected $sqlUpdate = "update bsc_participante set COD_CPF= '%s',COD_RG= '%s',DSC_Nome= '%s',DSC_Endereco= '%s',DSC_Bairro= '%s',DSC_Cidade= '%s',NUM_CEP= '%s',NUM_Fone= '%s',NUM_Celular= '%s',NUM_FAX= '%s',DSC_Profissao_Especialidade= '%s',DSC_Email= '%s',NUM_Registro= '%s',COD_Tipo_Estado= '%s',ID_BSC_Empresa= '%s',ID_BSC_Profissao= '%s' where ID_Participante = '%s'";
-    protected $sqlDelete = "delete from bsc_participante where ID_Participante = '%s'";
-    protected $sqlSelect = "select * from bsc_participante where 1=1 %s %s";
+    protected $sqlUpdate = "UPDATE bsc_participante SET COD_CPF= '%s',COD_RG= '%s',DSC_Nome= '%s',DSC_Endereco= '%s',DSC_Bairro= '%s',DSC_Cidade= '%s',NUM_CEP= '%s',NUM_Fone= '%s',NUM_Celular= '%s',NUM_FAX= '%s',DSC_Profissao_Especialidade= '%s',DSC_Email= '%s',NUM_Registro= '%s',COD_Tipo_Estado= '%s',ID_BSC_Empresa= '%s',ID_BSC_Profissao= '%s' WHERE ID_Participante = '%s'";
+    protected $sqlDelete = "DELETE FROM bsc_participante WHERE ID_Participante = '%s'";
+    protected $sqlSelect = "SELECT * FROM bsc_participante WHERE 1=1 %s %s";
 
+    protected $sqlinsertPartUs ="INSERT INTO bsc_participante(COD_CPF,COD_RG,DSC_Nome,DSC_Endereco,DSC_Bairro,DSC_Cidade,NUM_CEP,NUM_Fone,NUM_Celular,NUM_FAX,DSC_Profissao_Especialidade,DSC_Email,NUM_Registro,COD_Tipo_Estado,ID_BSC_Empresa,ID_BSC_Profissao) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')";
+    
     public function insert() {
         $sql = sprintf($this->sqlInsert, $this->getCOD_CPF(), $this->getCOD_RG(), $this->getDSC_Nome(), $this->getDSC_Endereco(), $this->getDSC_Bairro(), $this->getDSC_Cidade(), $this->getNUM_CEP(), $this->getNUM_Fone(), $this->getNUM_Celular(), $this->getNUM_FAX(), $this->getDSC_Profissao_Especialidade(), $this->getDSC_Email(), $this->getNUM_Registro(), $this->getCOD_Tipo_Estado(), $this->getID_BSC_Empresa(), $this->getID_BSC_Profissao());
         return $this->RunQuery($sql);
@@ -49,6 +51,11 @@ class aBsc_Participante extends mBsc_Participante {
         $this->getID_Participante($rs[0]['ID_BSC_Empresa']);
         $this->getID_Participante($rs[0]['ID_BSC_Profissao']);
         return $this;
+    }
+    
+    public function insertPartUs() {
+        $sql = sprintf($this->sqlinsertPartUs, $this->getCOD_CPF(), $this->getCOD_RG(), $this->getDSC_Nome(), $this->getDSC_Endereco(), $this->getDSC_Bairro(), $this->getDSC_Cidade(), $this->getNUM_CEP(), $this->getNUM_Fone(), $this->getNUM_Celular(), $this->getNUM_FAX(), $this->getDSC_Profissao_Especialidade(), $this->getDSC_Email(), $this->getNUM_Registro(), $this->getCOD_Tipo_Estado(), $this->getID_BSC_Empresa(), $this->getID_BSC_Profissao());
+        return $this->RunQuery($sql);
     }
 
 }
