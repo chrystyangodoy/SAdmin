91aa1730993ff18a4067e53a2b0f9017e0e49ec5
Vandame-PC <Vandame-PC@DESKTOP-TLP8JNK>
2015-09-05 10:31:53 -0300

    ParticipanteInsert. Validações;

--- js/ParticipanteInsert.js
+++ js/ParticipanteInsert.js
@@ -67,14 +67,54 @@ $(document).ready(function () {
     $('#NUM_FAX').mask('(00) 0000-0000');
     $('#NUM_FAX_Promotora').mask('(00) 0000-0000');
     $('#NUM_CEP').mask('00.000-000');
-    
+/*
+    $('#Cadastrar').tooltipster({
+        trigger: 'custom', // default is 'hover' which is no good here
+        onlyOne: false, // allow multiple tips to be open at a time
+        position: 'right'  // display the tips to the right of the element
+    });
+*/
+
     $('#Cadastrar').validate({
-        rules:{
-            DSC_Nome:{requered:true},
-            
+        rules: {
+            DSC_Nome: {required: true},
+            DSC_Email: {required: true, email: true},
+            COD_CPF: {required: true},
+            NUM_CEP: {required: true},
+            DSC_Cidade: {required: true},
+            DSC_Bairro: {required: true},
+            DSC_Endereco: {required: true}
         },
-        messages:{
-            
+        messages: {
+            DSC_Nome: {required: 'Preencha o campo nome'},
+            DSC_Email: {required: 'Informe o seu email', email: 'Ops, informe um email válido'},
+            COD_CPF: {required: 'Preencha o campo CPF'},
+            NUM_CEP: {required: 'Preencha o campo CEP'},
+            DSC_Cidade: {required: 'Preencha o campo Cidade'},
+            DSC_Bairro: {required: 'Preencha o campo Bairro'},
+            DSC_Endereco: {required: 'Preencha o campo Endereço'}
+
+        },
+        /*     errorPlacement: function (error, element) {
+         $(element).tooltipster('update', $(error).text());
+         $(element).tooltipster('show');
+         },
+         success: function (label, element) {
+         $(element).tooltipster('hide');
+         },*/
+        submitHandler: function (form) {
+            var dados = $(form).serialize();
+
+            $.ajax({
+                type: "POST",
+                url: "ParticipanteInsert.php",
+                data: dados,
+                success: function (data)
+                {
+                }
+            });
+
+            return false;
         }
     });
 });
