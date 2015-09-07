15d4d5092d9cab5710616eecf27e154d23cbbd2a
Vandame-PC <Vandame-PC@DESKTOP-TLP8JNK>
2015-09-07 10:16:28 -0300

    feito commit

--- js/ParticipanteInsert.js
+++ js/ParticipanteInsert.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
 
     function limpa_formulário_cep() {
@@ -45,15 +47,15 @@ $(document).ready(function () {
                     else {
                         //CEP pesquisado não foi encontrado.
                         limpa_formulário_cep();
-                        showAlert('error','CEP não encontrado.');
+                        showAlert('error', 'CEP não encontrado.');
                     }
                 });
             } //end if.
             else {
                 //cep é inválido.
                 limpa_formulário_cep();
-                showAlert('error','Formato de CEP inválido.');
-           }
+                showAlert('error', 'Formato de CEP inválido.');
+            }
         } //end if.
         else {
             //cep sem valor, limpa formulário.
@@ -103,19 +105,20 @@ $(document).ready(function () {
          $(element).tooltipster('hide');
          },*/
         submitHandler: function (form) {
-           
+
             //var dados = $(form).serialize();
-            
+
             $.ajax({
                 type: "POST",
                 url: "ParticipanteInsert.php",
                 data: dados,
                 success: function (data)
-                {if(data == 1){
-                        showAlert('success','Salvo.');
-                }else{
-                    showAlert('error','falha ao salvar.');
-                }
+                {
+                    if (data == 1) {
+                        showAlert('success', 'Salvo.');
+                    } else {
+                        showAlert('error', 'falha ao salvar.');
+                    }
                 }
             });
 
@@ -123,3 +126,39 @@ $(document).ready(function () {
         }
     });
 });
+
+jQuery.validator.addMethod("cpf", function (value, element) {
+    value = jQuery.trim(value);
+
+    value = value.replace('.', '');
+    value = value.replace('.', '');
+    cpf = value.replace('-', '');
+    while (cpf.length < 11)
+        cpf = "0" + cpf;
+    var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+    var a = [];
+    var b = new Number;
+    var c = 11;
+    for (i = 0; i < 11; i++) {
+        a[i] = cpf.charAt(i);
+        if (i < 9)
+            b += (a[i] * --c);
+    }
+    if ((x = b % 11) < 2) {
+        a[9] = 0
+    } else {
+        a[9] = 11 - x
+    }
+    b = 0;
+    c = 11;
+    for (y = 0; y < 10; y++)
+        b += (a[y] * c--);
+    if ((x = b % 11) < 2) {
+        a[10] = 0;
+    } else {
+        a[10] = 11 - x;
+    }
+    if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg))
+        return this.optional(element) || false;
+    return this.optional(element) || true;
+}, "Informe um CPF válido."); // Mensagem padrão    
