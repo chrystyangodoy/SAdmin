3a47e0d611fe22ff26b13299a7a920c9e9be264f
Vandame-PC <Vandame-PC@DESKTOP-TLP8JNK>
2015-09-07 15:03:18 -0300

    validações da tela de cadastro do participante.

--- js/ParticipanteInsert.js
+++ js/ParticipanteInsert.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function () {
 
     function limpa_formulário_cep() {
@@ -81,7 +79,7 @@ $(document).ready(function () {
         rules: {
             DSC_Nome: {required: true},
             DSC_Email: {required: true, email: true},
-            COD_CPF: {required: true},
+            COD_CPF: {required: true, cpfValidacao: true, cpfCadastrado: true},
             NUM_CEP: {required: true},
             DSC_Cidade: {required: true},
             DSC_Bairro: {required: true},
@@ -112,6 +110,7 @@ $(document).ready(function () {
                 type: "POST",
                 url: "ParticipanteInsert.php",
                 data: dados,
+                
                 success: function (data)
                 {
                     if (data == 1) {
@@ -125,40 +124,15 @@ $(document).ready(function () {
             return false;
         }
     });
+    
+    $("#COD_CPF").blur(function(){
+        cpf = $("#COD_CPF").val();
+        get = isCPFCadastrado(cpf);
+        
+        if(get == 0){
+            showAlert('error', 'CPF já Cadastrado');
+        }
+        
+    });
 });
 
-jQuery.validator.addMethod("cpf", function (value, element) {
-    value = jQuery.trim(value);
-
-    value = value.replace('.', '');
-    value = value.replace('.', '');
-    cpf = value.replace('-', '');
-    while (cpf.length < 11)
-        cpf = "0" + cpf;
-    var expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
-    var a = [];
-    var b = new Number;
-    var c = 11;
-    for (i = 0; i < 11; i++) {
-        a[i] = cpf.charAt(i);
-        if (i < 9)
-            b += (a[i] * --c);
-    }
-    if ((x = b % 11) < 2) {
-        a[9] = 0
-    } else {
-        a[9] = 11 - x
-    }
-    b = 0;
-    c = 11;
-    for (y = 0; y < 10; y++)
-        b += (a[y] * c--);
-    if ((x = b % 11) < 2) {
-        a[10] = 0;
-    } else {
-        a[10] = 11 - x;
-    }
-    if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg))
-        return this.optional(element) || false;
-    return this.optional(element) || true;
-}, "Informe um CPF válido."); // Mensagem padrão    
