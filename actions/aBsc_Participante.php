034c83ee05f7aced772721f6ddf68547ced46791
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-08 17:39:41 -0300

    Merge origin/master
    Ajuste para gerar usuário ao cadastrar o participante...
    Nova Classe para Envio de email...

--- actions/aBsc_Participante.php
+++ actions/aBsc_Participante.php
@@ -4,7 +4,7 @@ require './model/mBsc_Participante.php';
 
 class aBsc_Participante extends mBsc_Participante {
 
-    protected $sqlInsert = "INSERT INTO bsc_participante(COD_CPF,COD_RG,DSC_Nome,DSC_Endereco,DSC_Bairro,DSC_Cidade,NUM_CEP,NUM_Fone,NUM_Celular,NUM_FAX,DSC_Profissao_Especialidade,DSC_Email,NUM_Registro,COD_Tipo_Estado,ID_BSC_Empresa,ID_BSC_Profissao,ID_Usuario) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')";
+    protected $sqlInsert = "INSERT INTO bsc_participante(COD_CPF, COD_RG, DSC_Nome, DSC_Endereco, DSC_Bairro, DSC_Cidade, NUM_CEP, NUM_Fone, NUM_Celular, NUM_FAX, DSC_Profissao_Especialidade, DSC_Email, NUM_Registro, COD_Tipo_Estado, ID_BSC_Empresa, ID_BSC_Profissao, ID_Usuario) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')";
     protected $sqlUpdate = "UPDATE bsc_participante SET COD_CPF= '%s',COD_RG= '%s',DSC_Nome= '%s',DSC_Endereco= '%s',DSC_Bairro= '%s',DSC_Cidade= '%s',NUM_CEP= '%s',NUM_Fone= '%s',NUM_Celular= '%s',NUM_FAX= '%s',DSC_Profissao_Especialidade= '%s',DSC_Email= '%s',NUM_Registro= '%s',COD_Tipo_Estado= '%s',ID_BSC_Empresa= '%s',ID_BSC_Profissao= '%s' WHERE ID_Participante = '%s'";
     protected $sqlDelete = "DELETE FROM bsc_participante WHERE ID_Participante = '%s'";
     protected $sqlSelect = "SELECT * FROM bsc_participante WHERE 1=1 %s %s";
@@ -12,6 +12,7 @@ class aBsc_Participante extends mBsc_Participante {
     protected $sqlinsertPartUs = "INSERT INTO bsc_participante(COD_CPF,COD_RG,DSC_Nome,DSC_Endereco,DSC_Bairro,DSC_Cidade,NUM_CEP,NUM_Fone,NUM_Celular,NUM_FAX,DSC_Profissao_Especialidade,DSC_Email,NUM_Registro,COD_Tipo_Estado,ID_BSC_Empresa,ID_BSC_Profissao) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')";
 
     public function insert() {
+        $sql = sprintf($this->sqlInsert, $this->getCOD_CPF(), $this->getCOD_RG(), $this->getDSC_Nome(), $this->getDSC_Endereco(), $this->getDSC_Bairro(), $this->getDSC_Cidade(), $this->getNUM_CEP(), $this->getNUM_Fone(), $this->getNUM_Celular(), $this->getNUM_FAX(), $this->getDSC_Profissao_Especialidade(), $this->getDSC_Email(), $this->getNUM_Registro(), $this->getCOD_Tipo_Estado(), $this->getID_BSC_Empresa(), $this->getID_BSC_Profissao(), $this->getID_Usuario());
         return $this->RunQuery($sql);
     }
 
