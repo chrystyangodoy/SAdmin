f3907cf8e25648d8586a1c985cc7d907a99bf959
Victor Hugo <victorhugolgr@gmail.com>
2015-09-09 14:56:19 -0300

    Criação da área do Usuário.

--- AreaUsuario.php
+++ AreaUsuario.php
@@ -1,3 +1,10 @@
 <?php
+include_once './smarty.php';
+require_once './config/FeedbackMessage.php';
 
+$FeedbackMessage = new FeedbackMessage();
 
+$smarty->assign("msg", $FeedbackMessage->getMsg());
+$smarty->assign("type", $FeedbackMessage->getType());
+
+$smarty->display('./View/AreaUsuario.html');
