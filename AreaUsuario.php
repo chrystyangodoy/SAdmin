7427666e95d6c7abe892d0d2023c9c1c753ef1a6
Victor Hugo <victorhugolgr@gmail.com>
2015-09-10 17:31:16 -0300

    Alteração da área do Usuário.

--- AreaUsuario.php
+++ AreaUsuario.php
@@ -1,12 +1,28 @@
 <?php
+
+//-----INCLUDES---------------------------------------------
 include_once './smarty.php';
 require_once './config/FeedbackMessage.php';
+require_once './actions/aEvt_Evento.php';
+//----------------------------------------------------------
 session_start();
+//----------------------------------------------------------
+//-----------VARIÁVEIS--------------------------------------
 $FeedbackMessage = new FeedbackMessage();
+$evento = new aEvt_Evento();
+//----------------------------------------------------------
+
+$lista = $evento->SelectEventosEmdia();
 
 
+
+
+
+//-------------------SMARTY---------------------------------
 $smarty->assign("dscUser", $_SESSION['DSC_Login']);
 $smarty->assign("msg", $FeedbackMessage->getMsg());
 $smarty->assign("type", $FeedbackMessage->getType());
+$smarty->assign("lista", $lista);
 
 $smarty->display('./View/AreaUsuario.html');
+//----------------------------------------------------------
--- actions/aEvt_Evento.php
+++ actions/aEvt_Evento.php
@@ -50,5 +50,9 @@ class aEvt_Evento extends mEvt_Evento {
         $this->setID_EVT($rs[0]['COD_Tipo_Estado_promotora']);
         return $this;
     }
+    
+    public function SelectEventosEmdia(){
+        return $this->select(" DT_Inicio > CURRENT_DATE()");
+    }
 
 }
