2a2e8ce94713e85977d81e45e2311837190e2c56
Chrystyan <Chrystyan@NoteChrystyan>
2015-09-17 23:25:43 -0300

    Merge origin/master Inclusão do CRUD para Categoria Evento

---
+++ EventoCategoriaInsert.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'smarty.php';
+require_once ('./config/configs.php');
+require ('./actions/aEvt_Evento_Categoria.php');
+require_once './actions/aEvt_Evento.php';
+require_once './config/FeedbackMessage.php';
+session_start();
+$FeedbackMessage = new FeedbackMessage();
+$eventoCategoria = new aEvt_Evento_Categoria();
+$evento = new aEvt_Evento();
+
+$config = new configs();
+
+if (isset($_POST['Cadastrar'])) {
+    $idUnico = $config->idUnico();
+    
+    $eventoCategoria->setID_Evento_Categoria($idUnico);
+    $eventoCategoria->setDSC_Nome($_POST['DSC_Nome']);
+    $eventoCategoria->setVLR_Inscricao($_POST['VLR_Inscricao']);
+    $eventoCategoria->setDT_Inicio_Valor($_POST['DT_Inicio_Valor']);
+    $eventoCategoria->setDT_Fim_Valor($_POST['DT_Fim_Valor']);
+    $eventoCategoria->setID_EVT_Evento($_POST['ID_EVT_Evento']);
+    
+    $evento->insert();
+    
+    $FeedbackMessage->setMsg("Evento inserido com sucesso!");
+}
+
+$smarty->assign("dscUser", $_SESSION['DSC_Login']);
+$smarty -> assign("listEvento",$evento->select());
+$smarty->assign("msg", $FeedbackMessage->getMsg());
+$smarty->assign("type", $FeedbackMessage->getType());
+
+$smarty->display('./View/EventoInsert.html');
---
+++ EventoCategoriaList.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once './smarty.php';
+require ('./actions/aEvt_Evento_Categoria.php');
+$eventoCateg = new aEvt_Evento_Categoria();
+session_start();
+require_once './config/FeedbackMessage.php';
+$FeedbackMessage = new FeedbackMessage();
+
+if (isset($_GET['del'])) {
+    $eventoCateg->setID_Evento_Categoria($_GET['del']);
+    $eventoCateg->delete();
+}
+
+$smarty->assign("dscUser", $_SESSION['DSC_Login']);
+$smarty->assign("msg", $FeedbackMessage->getMsg());
+$smarty->assign("type", $FeedbackMessage->getType());
+
+$smarty->assign("lista", $eventoCateg->select());
+$smarty->display('./View/EventoCategoriaList.html');
