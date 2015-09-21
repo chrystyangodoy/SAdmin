b480a9b6c700b119c60e30c91c4e50d5a2743795
Vandame-PC <Vandame-PC@Vandame-PC>
2015-09-20 23:49:32 -0300

    Configuração de e-mail

--- config/eMail.php
+++ config/eMail.php
@@ -12,6 +12,9 @@
  * @author Chrystyan Godoy <F1r3Black0ut>
  */
 class eMail {
+    
+    
+    private $smtp = 'smtp.gmail.com';
 
     public function sendEmail($remetente, $destinatario, $assunto, $mensagem) {
 
@@ -22,7 +25,7 @@ class eMail {
         $headers .= "Content-type: text/html; charset=iso-8859-1\n";
         $headers .= "From: " . $remetente . "\r\n"; // remetente
         $headers .= "Return-Path: " . $remetente . "\r\n"; // return-path
-        
+
         if (mail($destinatario, $assunto, $mensagem, $headers)) {
             echo "Mensagem enviada com sucesso";
             return true;
@@ -31,6 +34,55 @@ class eMail {
             return false;
         }
         return false;
+    }
+
+    public function enviarEMail($detinatario,$nome,$assunto,$mensagem) {
+        // Inclui o arquivo class.phpmailer.php localizado na pasta phpmailer
+        require_once './phpMailer/class.phpmailer.php';
+        require_once './phpMailer/PHPMailerAutoload.php';
+        // Inicia a classe PHPMailer
+        $mail = new PHPMailer();
+        // Define os dados do servidor e tipo de conexão
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        $mail->IsSMTP(); // Define que a mensagem será SMTP
+        $mail->Host = $this->smtp; // Endereço do servidor SMTP
+        //$mail->SMTPAuth = true; // Usa autenticação SMTP? (opcional)
+        //$mail->Username = 'seumail@dominio.net'; // Usuário do servidor SMTP
+        //$mail->Password = 'senha'; // Senha do servidor SMTP
+        // Define o remetente
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        $mail->From = $detinatario; // Seu e-mail
+        $mail->FromName = $nome; // Seu nome
+        // Define os destinatário(s)
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        $mail->AddAddress('victorhugolgr@gmail.com', 'Victor Hugo');
+        //$mail->AddAddress('ciclano@site.net');
+        //$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
+        //$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
+        // Define os dados técnicos da Mensagem
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        $mail->IsHTML(true); // Define que o e-mail será enviado como HTML
+        //$mail->CharSet = 'iso-8859-1'; // Charset da mensagem (opcional)
+        // Define a mensagem (Texto e Assunto)
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        $mail->Subject = $assunto; // Assunto da mensagem
+        $mail->Body = $mensagem;
+        $mail->AltBody = "Este é o corpo da mensagem de teste, em Texto Plano! \r\n :)";
+        // Define os anexos (opcional)
+        // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        //$mail->AddAttachment("c:/temp/documento.pdf", "novo_nome.pdf");  // Insere um anexo
+        // Envia o e-mail
+        $enviado = $mail->Send();
+        // Limpa os destinatários e os anexos
+        $mail->ClearAllRecipients();
+        $mail->ClearAttachments();
+        // Exibe uma mensagem de resultado
+        if ($enviado) {
+            echo "E-mail enviado com sucesso!";
+        } else {
+            echo "Não foi possível enviar o e-mail.";
+            echo "<b>Informações do erro:</b> " . $mail->ErrorInfo;
+        }
     }
 
 }
