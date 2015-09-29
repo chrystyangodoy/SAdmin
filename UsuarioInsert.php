f7778d2e9a953b632dbe02aafbc0a4687a508c1d
Chrystyan Cleyton de Araújo Godoy <chrystyangodoy@gmail.com>
2015-09-29 11:06:45 -0300

    Merge origin/master
    Ajuste no Usuário - Correção newID do usuário.
    Alter Estado

--- UsuarioInsert.php
+++ UsuarioInsert.php
@@ -1,12 +1,20 @@
 <?php
 require_once 'smarty.php';
-
+require_once ('./config/configs.php');
+session_start();
 require ('./actions/aUsuario.php');
 require ('./actions/aGrupoUsuario.php');
+
+$config = new configs();
+
 $grupo = new aGrupoUsuario();
 $user= new aUsuario();
 
+
 if(isset($_POST['Cadastrar'])){
+    $idUnico = $config->idUnico();
+    $user->setID_Usuario($idUnico);
+    
     $user->setDSC_Login($_POST['DSC_Login']);
     $user->setDSC_Senha($_POST['DSC_Senha']);
     $user->setDTM_Inicio($_POST['DTM_Inicio']);
