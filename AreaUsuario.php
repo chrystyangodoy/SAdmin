80ea0745805d175c5d50f211f126ee2e9c36f7ee
Chrystyan Cleyton de Araújo Godoy <chrystyangodoy@gmail.com>
2015-10-02 00:07:13 -0300

    Merge origin/master Ajuste na inscrição do participante no evento. Teste de existência do evento para o participante da sessão... Foi ajustado o metodo selectNotExistsEvt, idUser não é igual ao id_Participante.

--- AreaUsuario.php
+++ AreaUsuario.php
@@ -23,23 +23,8 @@ if (isset($_GET['idevt']))
     $evtPart->setID_EVT_Evento_Pariticipante($config->idUnico());
     $evtPart->setDSC_Nome_Crachav($partic->getDSC_Nome());
     $evtPart->setCOD_Barras_Cracha($partic->getCOD_CPF());
-//    $evtPart->setVLR_Total();
-//    $evtPart->setVLR_Total_Inscricao();
-//    $evtPart->setQTD_CargaHoraria_Realizada();
-//    $evtPart->setCOD_Nivel_Participante();
-//    $evtPart->setID_EVT_Pagamento();
-//    $evtPart->setID_EVT_Categoria();
     $evtPart->setID_EVT_Evento($IDEvto);
     $evtPart->setID_BSC_Participante($partic->getID_Participante());
-//    $evtPart->setID_EVT_Participante_Pai();
-//    $evtPart->setCOD_Tipo_SIT_Certificado();
-//    $evtPart->setDTM_Entrega_Certificado();
-//    $evtPart->setID_SEG_DetalheTransacao();
-//    $evtPart->setSIT_EH_Parcelado();
-//    $evtPart->setID_EVT_EventoGrupo();
-//    $evtPart->setCOD_TipoSituacao_Material();
-//    $evtPart->setDTM_EntregaMaterial();
-//    $evtPart->setCOD_InscricaoExterno();
 
     $evtPart->insert();
 }
@@ -48,13 +33,14 @@ $FeedbackMessage = new FeedbackMessage();
 require_once './actions/aEvt_Evento_Participante.php';
 $evtPart = new aEvt_Evento_Participante();
 
-$lista = $evtPart->select();
+$lista = $evtPart->SelectEvtPartc($_SESSION['ID_Usuario']);
 
 $smarty->assign("dscUser", $_SESSION['DSC_Login']);
 $smarty->assign("msg", $FeedbackMessage->getMsg());
 $smarty->assign("type", $FeedbackMessage->getType());
 $smarty->assign("lista", $lista);
 
-$smarty->display('./View_Participante/AreaParticipante.html');
+$smarty->display('./View/AreaUsuario.html');
+//$smarty->display('./View_Participante/AreaParticipante.html');
 
 //----------------------------------------------------------
