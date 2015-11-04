3586839644e23c0b471a3f63d8f489a7b6e02b41
Victor Hugo <victorhugolgr@gmail.com>
2015-11-04 11:27:37 -0300

    area participante

--- config/ValidaSessao.php
+++ config/ValidaSessao.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($_SESSION['ID_Usuario'] == null || $_SESSION['ID_Usuario'] == 0 || !isset($_SESSION['ID_Usuario'])) {
+if (!isset($_SESSION['ID_Usuario'])) {
     header("Location: Index.php");
     die();
 }
