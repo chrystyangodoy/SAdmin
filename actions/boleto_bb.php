e88b169817cfe18618cd2203a7c70ab8bfc718ad
Chrystyan Cleyton de Araújo Godoy <chrystyangodoy@gmail.com>
2016-01-14 11:54:10 -0200

    Alteração no boleto para pegar as informações da conta diretamente da tabela.

--- actions/boleto_bb.php
+++ actions/boleto_bb.php
@@ -1,4 +1,5 @@
 <?php
+
 // | Captura de informações para geração do boleto                                                          |
 // | require_once das actions necessárias  
 require_once './actions/aEvt_Evento.php';
@@ -24,9 +25,12 @@ $Evento->setID_EVT($DadosEvento->getID_EVT_Evento());
 $Evento->load();
 
 $CodBanco = $Evento->getID_Banco();
-if($CodBanco==0 ||$CodBanco==NULL){
-    $Banco->setID('1');    
-}Else{
+if ($CodBanco == 0 || $CodBanco == NULL)
+{
+    $Banco->setID('1');
+}
+Else
+{
     $Banco->setID($CodBanco);
 }
 $Banco->load();
@@ -72,7 +76,7 @@ $dadosboleto["endereco1"] = $Partic->getDSC_Endereco();
 require_once ('./actions/atb_Tipo_Estado.php');
 $Estado = new atb_Tipo_Estado();
 $Estado->setCOD_TIPOEstado($Partic->getCOD_Tipo_Estado());
-$dadosboleto["endereco2"] = $Partic->getDSC_Cidade()." / ".$Estado->getDSC_Nome()." - ".$Partic->getNUM_CEP();
+$dadosboleto["endereco2"] = $Partic->getDSC_Cidade() . " / " . $Estado->getDSC_Nome() . " - " . $Partic->getNUM_CEP();
 
 // INFORMACOES PARA O CLIENTE
 $dadosboleto["demonstrativo1"] = "Pagamento em qualquer Agência Banco do Brasil";
@@ -89,7 +93,6 @@ $dadosboleto["instrucoes4"] = "";
 //$dadosboleto["instrucoes2"] = "- Não receber após o vencimento";
 //$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: xxxx@xxxx.com.br";
 //$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
-
 // DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
 $dadosboleto["quantidade"] = "";
 $dadosboleto["valor_unitario"] = "";
@@ -98,17 +101,36 @@ $dadosboleto["especie"] = "R$";
 $dadosboleto["especie_doc"] = "DM";
 // ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
 // DADOS DA SUA CONTA - BANCO DO BRASIL
-$dadosboleto["agencia"] = "08788"; // Num da agencia, sem digito
-$dadosboleto["conta"] = "26095";  // Num da conta, sem digito
+//$dadosboleto["agencia"] = "08788"; // Num da agencia, sem digito
+$dadosboleto["agencia"] = $Banco->getAgencia();
+//$dadosboleto["conta"] = "26095";  // Num da conta, sem digito
+$dadosboleto["conta"] = $Banco->getConta();
 // DADOS PERSONALIZADOS - BANCO DO BRASIL
-$dadosboleto["convenio"] = "7777777";  // Num do convênio - REGRA: 6 ou 7 ou 8 dígitos
-$dadosboleto["contrato"] = "999999"; // Num do seu contrato
-$dadosboleto["carteira"] = "18";
-$dadosboleto["variacao_carteira"] = "-019";  // Variação da Carteira, com traço (opcional)
+//$dadosboleto["convenio"] = "7777777";// Num do convênio - REGRA: 6 ou 7 ou 8 dígitos
+$dadosboleto["convenio"] = $Banco->getConvenio();
+//$dadosboleto["contrato"] = "999999"; // Num do seu contrato
+$dadosboleto["contrato"] = $Banco->getContrato();
+//$dadosboleto["carteira"] = "18";
+$dadosboleto["carteira"] = $Banco->getCarteira();
+//$dadosboleto["variacao_carteira"] = "-019";  // Variação da Carteira, com traço (opcional)
+$dadosboleto["variacao_carteira"] = $Banco->getVariacao_Carteira();
 // TIPO DO BOLETO
-$dadosboleto["formatacao_convenio"] = "7"; // REGRA: 8 p/ Convênio c/ 8 digitos, 7 p/ Convênio c/ 7 d�gitos, ou 6 se Convênio c/ 6 dígitos
-$dadosboleto["formatacao_nosso_numero"] = "1"; // REGRA: Usado apenas p/ Convênio c/ 6 dígitos: informe 1 se for NossoNúmero de até 5 dígitos ou 2 para opção de até 17 dígitos
-
+//$dadosboleto["formatacao_convenio"] = "7"; // REGRA: 8 p/ Convênio c/ 8 digitos, 7 p/ Convênio c/ 7 d�gitos, ou 6 se Convênio c/ 6 dígitos
+$dadosboleto["formatacao_convenio"] = strlen($Banco->getConvenio());
+//$dadosboleto["formatacao_nosso_numero"] = "1"; // REGRA: Usado apenas p/ Convênio c/ 6 dígitos: informe 1 se for NossoNúmero de até 5 dígitos ou 2 para opção de até 17 dígitos
+$nro_dig_Convenio = strlen($Banco->getConvenio());
+$nro_dig_NossoNumero = strlen($Banco->getnumero_documento());
+if ($nro_dig_Convenio == 6)
+{
+    if ($nro_dig_NossoNumero <= 5)
+    {
+        $dadosboleto["formatacao_nosso_numero"] = 1;
+    }
+    Else
+    {
+        $dadosboleto["formatacao_nosso_numero"] = 2;
+    }
+}
 /*
   #################################################
   DESENVOLVIDO PARA CARTEIRA 18
@@ -127,21 +149,24 @@ $dadosboleto["formatacao_nosso_numero"] = "1"; // REGRA: Usado apenas p/ Convên
   #################################################
  */
 $IsPromotora = $Evento->getisPromotora();
-if($IsPromotora){
-$NomeEmpresa = $Evento->getDSC_Nome_Promotora();
-$cnpjEmpresa = $Evento->getCOD_CNPJ_Promotora();
-$Endereço =  $LocalEvento->getDSC_Nome()." - ".$LocalEvento->getDSC_Endereco()." - ".$LocalEvento->getDSC_Bairro()." Telefone: ".$LocalEvento->getNUM_Fone();
-$CidadeEstado = $LocalEvento->getDSC_Cidade();    
-} Else{
-require_once './actions/aBsc_Empresa.php';
-$BSC_Empresa = new aBsc_Empresa();
-$BSC_Empresa->setID_Empresa($Evento->getID_Empresa());
-$BSC_Empresa->load();
+if ($IsPromotora)
+{
+    $NomeEmpresa = $Evento->getDSC_Nome_Promotora();
+    $cnpjEmpresa = $Evento->getCOD_CNPJ_Promotora();
+    $Endereço = $LocalEvento->getDSC_Nome() . " - " . $LocalEvento->getDSC_Endereco() . " - " . $LocalEvento->getDSC_Bairro() . " Telefone: " . $LocalEvento->getNUM_Fone();
+    $CidadeEstado = $LocalEvento->getDSC_Cidade();
+}
+Else
+{
+    require_once './actions/aBsc_Empresa.php';
+    $BSC_Empresa = new aBsc_Empresa();
+    $BSC_Empresa->setID_Empresa($Evento->getID_Empresa());
+    $BSC_Empresa->load();
 
-$NomeEmpresa = $BSC_Empresa->getDSC_RazaoSocial();
-$cnpjEmpresa = $BSC_Empresa->getCOD_CNPJ();
-$EndereçoEvento =  $LocalEvento->getDSC_Nome()." - ".$LocalEvento->getDSC_Endereco()." - ".$LocalEvento->getDSC_Bairro()." Telefone: ".$LocalEvento->getNUM_Fone();
-$CidadeEstadoEvento = $LocalEvento->getDSC_Cidade();
+    $NomeEmpresa = $BSC_Empresa->getDSC_RazaoSocial();
+    $cnpjEmpresa = $BSC_Empresa->getCOD_CNPJ();
+    $EndereçoEvento = $LocalEvento->getDSC_Nome() . " - " . $LocalEvento->getDSC_Endereco() . " - " . $LocalEvento->getDSC_Bairro() . " Telefone: " . $LocalEvento->getNUM_Fone();
+    $CidadeEstadoEvento = $LocalEvento->getDSC_Cidade();
 }
 
 // SEUS DADOS
