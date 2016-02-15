b0fbfc8be6ee0216161b4baf788bd288d7d30805
Chrystyan Cleyton de Araújo Godoy <chrystyangodoy@gmail.com>
2016-02-15 01:31:19 -0200

    Envio de email ao mudar Status de Pagto da inscrição...

--- getStatusPagtoEventoID.php
+++ getStatusPagtoEventoID.php
@@ -7,5 +7,22 @@ if (isset($_REQUEST['ID_Pagamento']) && isset($_REQUEST['COD_Tipo_Situacao_Pagam
     $Pagto->setID_Pagamento($_REQUEST['ID_Pagamento']);
     $Pagto->setCOD_Tipo_Situacao_Pagamento($_REQUEST['COD_Tipo_Situacao_Pagamento']);
     $Pagto->updateStatus();
+    
+    $IDEvento = $Pagto->getID_EVT_Evento();
+    require_once './actions/aEvt_Evento_Participante.php';
+    $EvtPartc = new aEvt_Evento_Participante();
+    $EvtPartc->setID_EVT_Evento($IDEvento);
+    $EvtPartc->load();
+    
+    require ('./actions/aBsc_Participante.php');
+    $partic = new aBsc_Participante();
+    
+    require_once './config/eMail.php';
+                $emailObj = new eMail();
+                $envio = $emailObj->enviarEMail($partic->getDSC_Email(), $partic->getDSC_Nome(), $ass, $msg);
+                $msgFeedMessage = 'Confirmação de Pagamento enviada com sucesso.';
+                $FeedbackMessage->setMsg($msgFeedMessage);
+                die();
+    
 }
 echo 1;
