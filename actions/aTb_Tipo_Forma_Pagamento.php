7d9449ad77d0782312f825dcdde98e13e1f04d63
Chrystyan <Chrystyan@NoteChrystyan>
2016-07-05 11:52:03 -0300

    Implementação Tabela de Condição de Pagamento. Já utilizada na geração do boleto de acordo com seleção durante a inscrição no evento. Ajuste no Nro do documento no banco e boletos gerados. Implementando o parcelamento (Boletos várias parcelas).

--- actions/aTb_Tipo_Forma_Pagamento.php
+++ actions/aTb_Tipo_Forma_Pagamento.php
@@ -4,10 +4,10 @@ require './model/mTb_Tipo_Forma_Pagamento.php';
 
 class aTb_Tipo_Forma_Pagamento extends mTb_Tipo_Forma_Pagamento {
 
-    protected $sqlInsert = "INSERT INTO Tb_Tipo_Forma_Pagamento ( COD_Tipo_Forma_Pagamento, DSC_Nome, DSC_Descricao,Nro_Parcelas,Dias_Vencimento)  VALUES ('%s','%s','%s','%s','%s')";
-    protected $sqlUpdate = "UPDATE Tb_Tipo_Forma_Pagamento SET DSC_Nome='%s',DSC_Descricao = '%s',Nro_Parcelas = '%s',Dias_Vencimento = '%s' WHERE COD_Tipo_Forma_Pagamento = '%s'";
-    protected $sqlDelete = "DELETE FROM Tb_Tipo_Forma_Pagamento WHERE COD_Tipo_Forma_Pagamento = '%s'";
-    protected $sqlSelect = "select * from Tb_Tipo_Forma_Pagamento where 1=1 %s %s";
+    protected $sqlInsert = "INSERT INTO tb_tipo_forma_pagamento ( COD_Tipo_Forma_Pagamento, DSC_Nome, DSC_Descricao,Nro_Parcelas,Dias_Vencimento)  VALUES ('%s','%s','%s','%s','%s')";
+    protected $sqlUpdate = "UPDATE tb_tipo_forma_pagamento SET DSC_Nome='%s',DSC_Descricao = '%s',Nro_Parcelas = '%s',Dias_Vencimento = '%s' WHERE COD_Tipo_Forma_Pagamento = '%s'";
+    protected $sqlDelete = "DELETE FROM tb_tipo_forma_pagamento WHERE COD_Tipo_Forma_Pagamento = '%s'";
+    protected $sqlSelect = "select * from tb_tipo_forma_pagamento where 1=1 %s %s";
 
     public function insert() {
         $sql = sprintf($this->sqlInsert, $this->getCOD_Tipo_Forma_Pagamento(),$this->getDSC_Nome(), $this->getDSC_Descricao(),$this->getNro_Parcelas(),$this->getDias_Vencimento());
@@ -30,7 +30,7 @@ class aTb_Tipo_Forma_Pagamento extends mTb_Tipo_Forma_Pagamento {
     }
 
     public function load() {
-        $rs = $this->select(sprintf("and COD_Tipo_Forma_Pagamento='%s'", $this->getCOD_Tipo_Forma_Pagamento()));
+        $rs = $this->select(sprintf(" and COD_Tipo_Forma_Pagamento='%s'", $this->getCOD_Tipo_Forma_Pagamento()));
         $this->setCOD_Tipo_Forma_Pagamento($rs[0]['COD_Tipo_Forma_Pagamento']);
         $this->setDSC_Nome($rs[0]['DSC_Nome']);
         $this->setDSC_Descricao($rs[0]['DSC_Descricao']);
